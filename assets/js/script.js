6099995dafc9ea70b7483775dbd04b84e09bbc64
Mouneeshwari Panneerselvam <mouneeshwari.panneerselvam@gmail.com>
2024-10-04 22:16:56 +0000

    Add EventListeners to all the buttons and set initial timer, score values

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
+/*jshint esversion: 6 */
+
+document.addEventListener('DOMContentLoaded', function(){
+    //References to the buttons and other dom elements
+    const startgamebtn= document.getElementById('startgamebtn');
+    const playagainbtn=document.getElementById('playagainbtn');
+    const cardcontainer=document.querySelector('.card-container');
+    const cards=document.querySelectorAll('.card');
+    const endgamebtn=document.getElementById('endgamebtn');
+    const howToPlaybtn = document.getElementById('howToPlaybtn');
+    const instructions = document.getElementById('instructions');
+    let score=0;
+    let timer=120; //setting initial timer to 2 mins(2 mins * 60 secs)
+}
+
 function shuffle() {
 
 }
@@ -43,5 +58,5 @@ function resetGame() {
 }
 
 function checkGameEnd() {
-    
+
 }
